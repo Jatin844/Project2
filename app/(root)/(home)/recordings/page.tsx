d8fad8fddc868ac448f6e24fbea2e04b265857e6
Jatin844 <jatinpareek80@gamil.com>
2024-07-05 11:58:06 +0530

    nmm

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -5,7 +5,7 @@ const PreviousPage = () => {
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Recordings</h1>
 
-      {/* <CallList type="recordings" /> */}
+      <CallList type="recordings" />
     </section>
   );
 };
